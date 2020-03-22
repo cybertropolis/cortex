9f9a90c55649b2532d87629089364c5879f88ec4
Rafael <rafaelluisdacostacoelho@gmail.com>
2020-03-21 21:05:12 -0300

    Add the neural network with feedforward.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cortex
+# Neural Network FeedForward
+
 A simple neural network with feedfoward that uses javascript and p5.
+
+The output result can be visualized in chrome console.
---
+++ sketch.js
@@ -0,0 +1,14 @@
+var train = true;
+
+function setup() {
+    createCanvas(500, 500);
+    background(0);
+
+    var nn = new NeuralNetwork(1, 3, 5);
+    var arr = [1, 2];
+    nn.feedforward(arr);
+}
+
+function draw() {
+
+}
